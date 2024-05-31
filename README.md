e0e24e9ecceb9a26824075621c4ae0ef10f939ff
atamayo <atamayoquintero@gmail.com>
2024-05-31 09:12:39 -0500

    feat: add Query and Mutation Examples

---
+++ README.md
@@ -0,0 +1,43 @@
+# GraphQL Server
+This project is an example of how to set up a GraphQL server using `apollo-server` and a local JSON database using `json-server`.
+
+## Requirements
+- Node.js (version 20.12.0)
+- npm (Node.js package manager)
+
+## Installation
+1. Clone the repository:
+
+```sh
+git clone <REPOSITORY_URL>
+cd graphql-Server
+```
+
+2. Install the project dependencies:
+
+```bash
+npm install
+```
+
+## Configuration
+The db.json file contains the JSON database data. You can edit this file to add, modify, or delete persons.
+
+## Usage
+Start the GraphQL Server
+To start the GraphQL server, run the following command:
+```bash
+npx nodemon index.js
+```
+
+The GraphQL server will be available at http://localhost:4000/, and you can use GraphQL Playground to interact with the API.
+
+## Start the JSON Server
+To start the JSON server, run the following command:
+```bash
+npm run json-server
+```
+The JSON server will be available at http://localhost:3000/persons.
+
+## Endpoints
+GraphQL Playground: http://localhost:4000/
+JSON Database: http://localhost:3000/persons
--- index.js
+++ index.js
@@ -1,41 +1,23 @@
 import { gql, ApolloServer } from 'apollo-server'
-
-import { persons } from './db.js'
 import { v1 as uuid } from 'uuid'
-// const persons = [
-//   {
-//     name: 'Midu',
-//     phone: '034-1234567',
-//     street: 'Calle Frontend',
-//     city: 'Barcelona',
-//     id: '1',
-//   },
-//   {
-//     name: 'Youseff',
-//     phone: '044-123456',
-//     street: 'Avenida Fullstack',
-//     city: 'Mataro',
-//     id: '2',
-//   },
-//   {
-//     name: 'Itzi',
-//     street: 'Pasaje Testing',
-//     city: 'Ibiza',
-//     id: '3',
-//   },
-// ]
+import axios from 'axios'
 
 const typeDefs = gql`
+  enum YesNo {
+    YES
+    NO
+  }
+
   type Query {
     personCount: Int!
-    persons: [Person]!
+    persons(phone: YesNo): [Person]!
     person(id: ID!): Person
   }
 
   type Person {
     id: ID!
     name: String!
-    phone: String!
+    phone: String
     address: Address!
   }
 
@@ -44,19 +26,47 @@ const typeDefs = gql`
     city: String!
   }
 
+  input PersonInput {
+    id: ID!
+    name: String
+    phone: String
+    street: String
+    city: String
+  }
+
+  type DeletePerson {
+    success: String!
+    message: String!
+  }
+
   type Mutation {
-    addPerson(name: String!, phone: String!, street: String!, city: String!): Person!
+    addPerson(name: String!, phone: String, street: String!, city: String!): Person!
+    # editPerson(id: String, name: String, phone: String, street: String, city: String): Person!
+    editPerson(person: PersonInput!): Person!
+    deletePerson(id: ID!): DeletePerson!
+    editPhone(id: ID!, phone: String!): Person
   }
 `
 
 const resolvers = {
   Query: {
-    personCount: () => persons.length,
-    persons: () => persons,
-    person: (parent, args) => {
+    personCount: async () => {
+      const { data: persons } = await axios.get(`http://localhost:3000/persons`)
+      return persons.length
+    },
+    persons: async (parent, args) => {
+      const { data: persons } = await axios.get(`http://localhost:3000/persons`)
+      if (!args.phone) return persons
+
+      const hasPhone = (person) => (args.phone === 'YES' ? person.phone : !person.phone)
+      return persons.filter(hasPhone)
+    },
+    person: async (parent, args) => {
+      const { data: persons } = await axios.get(`http://localhost:3000/persons`)
       return persons.find((person) => person.id === args.id)
     },
   },
+
   Person: {
     // address: (parent) => `${parent.street}, ${parent.city}`,
     address: (parent) => {
@@ -66,11 +76,54 @@ const resolvers = {
       }
     },
   },
+
   Mutation: {
-    addPerson: (parent, args) => {
-      const person = { ...args, id: uuid() }
-      persons.push(person)
-      return person
+    addPerson: async (parent, args) => {
+      try {
+        const newPerson = { ...args, id: uuid() }
+        const { data: person } = await axios.post(`http://localhost:3000/${'persons'}`, newPerson)
+        return person
+      } catch (error) {
+        throw new Error(`Failed to update phone: ${error.message}`)
+      }
+    },
+    editPerson: async (parent, args) => {
+      try {
+        // const updateFields = { ...args }
+        // delete updateFields.id
+        const { id, ...updateFields } = args.person
+        const { data: updatePerson } = await axios.patch(`http://localhost:3000/persons/${id}`, updateFields)
+
+        return updatePerson
+      } catch (error) {
+        throw new Error(`Failed to update phone: ${error.message}`)
+      }
+    },
+    deletePerson: async (parent, args) => {
+      try {
+        const { data: existingPerson } = await axios.get(`http://localhost:3000/persons/${args.id}`)
+        if (!existingPerson) {
+          throw new Error('Person not found')
+        }
+
+        await axios.delete(`http://localhost:3000/persons/${args.id}`)
+        return { success: true, message: 'Person deleted successfully' }
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          return { success: false, message: 'Person not found' }
+        }
+        throw new Error(`Failed to delete person: ${error.message}`)
+      }
+    },
+    editPhone: async (parent, args) => {
+      try {
+        const response = await axios.patch(`http://localhost:3000/persons/${args.id}`, {
+          phone: args.phone,
+        })
+        return response.data
+      } catch (error) {
+        throw new Error(`Failed to update phone: ${error.message}`)
+      }
     },
   },
 }
